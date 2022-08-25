a9393e25592428e4cde178bbaab2cb525df29ad4
Mohamed Abusrea <mohamed.abusrea@qbdvision.com>
2022-08-25 13:49:12 +0200

    prepare library for publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+// import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "ComponentLibrary",
+      // formats: ["es", "umd"],
+      fileName: (format) => `component-library.${format}.js`,
+    },
+  },
+  // @ts-ignore
+  rollupOptions: {
+    external: ["react", "react-dom"],
+    output: {
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+      },
+    },
+  },
+  plugins: [
+    react(),
+    // dts({
+    //   insertTypesEntry: true,
+    // }),
+  ],
   css: {
     preprocessorOptions: {
       less: {
