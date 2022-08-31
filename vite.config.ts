811bd5a2fcb352d653387c3ab19d2a2cb82fadec
Mohamed Abusrea <mohamed.abusrea@qbdvision.com>
2022-08-31 15:17:11 +0200

    - Add types to the dist package - Inline the css inside the js - Downgrade react peerDependencies to match our project - Add the ability to import style.css from the dist (if needed)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-// import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/lib/index.ts"),
       name: "ComponentLibrary",
-      // formats: ["es", "umd"],
       fileName: (format) => `component-library.${format}.js`,
     },
   },
@@ -24,8 +24,9 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    // dts({
-    //   insertTypesEntry: true,
-    // }),
+    dts({
+      insertTypesEntry: true,
+    }),
+    cssInjectedByJsPlugin(),
   ],
 });
