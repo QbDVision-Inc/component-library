4b5019433de8a09a22aa848f38feafee879d65d8
Mohamed Abusrea <mohamed.abusrea@qbdvision.com>
2022-10-12 19:04:19 +0200

    Fix class names in IconButton.tsx after updating them in Button component

--- src/components/atoms/IconButton/IconButton.tsx
+++ src/components/atoms/IconButton/IconButton.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from "react";
+import classNames from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 
-import classNames from "classnames";
 import style from "./IconButton.module.pcss";
 import buttonStyle from "../Button/Button.module.pcss";
 
 type IconButtonProps = {
-  icon?: IconDefinition;
+  icon?: IconProp;
   type?: "primary" | "secondary" | "tertiary";
   onClick?: () => void;
   disabled?: boolean;
@@ -31,10 +31,10 @@ const IconButton: FC<IconButtonProps> = ({
         className={classNames(
           buttonStyle.button,
           {
-            [buttonStyle.buttonPrimary]: type === "primary",
-            [buttonStyle.buttonSecondary]: type === "secondary",
+            [buttonStyle.primary]: type === "primary",
+            [buttonStyle.secondary]: type === "secondary",
             [style.iconButtonTertiary]: type === "tertiary",
-            [buttonStyle.buttonDisabled]: disabled,
+            [buttonStyle.disabled]: disabled,
           },
           style.iconButton
         )}
