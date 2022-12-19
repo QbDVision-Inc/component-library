5d7e55a2d258a4fd31241d6b5ae222223686c707
Mohamed Abusrea <mohamed.abusrea@qbdvision.com>
2022-12-19 17:25:21 +0200

    - Create InfoCard component - Create new build

--- dist/components/index.d.ts
+++ dist/components/index.d.ts
@@ -3,6 +3,7 @@ export { default as IconButton } from "../components/atoms/IconButton";
 export { default as Link } from "../components/atoms/Link";
 export { default as Logo } from "../components/atoms/Logo";
 export { default as Spinner } from "../components/atoms/Spinner";
+export { default as InfoCard } from "../components/atoms/InfoCard";
 export { default as ButtonGroup } from "../components/molecules/ButtonGroup";
 export { default as DropdownButton } from "../components/molecules/DropdownButton";
 export { default as NavBar } from "../components/molecules/NavBar";
---
+++ dist/utils.d.ts
@@ -0,0 +1,1 @@
+export declare const addCommasToNumber: (number?: number) => string | 0 | undefined;
