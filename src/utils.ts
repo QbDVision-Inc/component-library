1d0ad9d1138cffb031352adef533da4bd02e3225
Mohamed Abusrea <mohamed.abusrea@qbdvision.com>
2022-12-28 21:59:06 +0200

    - Fix an issue with the pluralized function to generate a proper ID - Increase version number

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,3 @@
-// @ts-ignore
-import pluralizeLib from "pluralize";
-
-const PLURALIZE_EXCEPTIONS_ARRAY: [string, string][] = [
-  ["ProcessParameterToIQA", "ProcessParameterToIQAs"],
-  ["ProcessParameterToIPA", "ProcessParameterToIPAs"],
-  ["ProcessParameterToFQA", "ProcessParameterToFQAs"],
-  ["ProcessParameterToFPA", "ProcessParameterToFPAs"],
-  ["MaterialAttributeToFQA", "MaterialAttributeToFQAs"],
-  ["MaterialAttributeToFPA", "MaterialAttributeToFPAs"],
-  ["MaterialAttributeToIQA", "MaterialAttributeToIQAs"],
-  ["MaterialAttributeToIPA", "MaterialAttributeToIPAs"],
-  ["FQAToGeneralAttributeRisk", "FQAToGeneralAttributeRisks"],
-  ["FPAToGeneralAttributeRisk", "FPAToGeneralAttributeRisks"],
-  ["RMPToUncertainty", "RMPToUncertainties"],
-  ["AcceptanceCriteriaRange", "AcceptanceCriteriaRanges"],
-  ["Specification", "Specifications"],
-];
-
-const PLURALIZE_EXCEPTIONS = new Map(PLURALIZE_EXCEPTIONS_ARRAY);
-
 export const convertToId = (someString: string) => {
   return (someString || "").toString().replace(/[^a-z0-9]/gim, "");
 };
@@ -42,37 +21,4 @@ export const convertToCamelCaseId = (someString?: string) => {
   returnVal = returnVal.charAt(0).toLowerCase() + returnVal.slice(1);
   returnVal = convertToId(returnVal);
   return returnVal;
-};
-
-export const pluralize: (
-  nounToPluralize: string,
-  count?: number | null
-) => string = (nounToPluralize, count = null) => {
-  if (!nounToPluralize) {
-    throw new TypeError("Noun to pluralize cannot be null");
-  }
-
-  const firstFewChars = nounToPluralize.substring(0, 3);
-  const lastFewChars = nounToPluralize.substring(
-    nounToPluralize.length - 3,
-    nounToPluralize.length
-  );
-
-  if (count !== null) {
-    return pluralizeLib(nounToPluralize, count);
-  }
-
-  if (PLURALIZE_EXCEPTIONS.has(nounToPluralize)) {
-    return PLURALIZE_EXCEPTIONS.get(nounToPluralize);
-  }
-
-  if (
-    firstFewChars === firstFewChars.toUpperCase() &&
-    lastFewChars === lastFewChars.toUpperCase()
-  ) {
-    // It's an acronym (i.e. IQA or IQAToFQA), so just add an "s"
-    return `${nounToPluralize}s`;
-  }
-
-  return pluralizeLib(nounToPluralize);
 };
